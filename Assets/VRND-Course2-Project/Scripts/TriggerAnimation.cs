648e81805ea43387406c4eef2624e050cefa8158
carlosepmendes <carlosepmendes@gmail.com>
2016-11-03 11:54:54 -0100

    Added the globe and its animations

--- Assets/VRND-Course2-Project/Scripts/TriggerAnimation.cs
+++ Assets/VRND-Course2-Project/Scripts/TriggerAnimation.cs
@@ -5,6 +5,7 @@ public class TriggerAnimation : MonoBehaviour {
     public string AnimationName;
     public Animator stateMachine;
 
+/*
     private bool created = false;
 
     void Awake() {
@@ -17,11 +18,11 @@ public class TriggerAnimation : MonoBehaviour {
     void Start() {
         if (created) {
             foreach (Camera c in GvrViewer.Instance.GetComponentsInChildren<Camera>()) {
-                c.enabled = false; // to use the Gvr SDK without adding cameras we have to disable them
+				c.enabled = false; // to use the Gvr SDK without adding cameras we have to disable them
             }
         }
-
     }
+*/
 
     void Update() {
         GvrViewer.Instance.UpdateState(); //need to update the data here otherwise we dont get mouse clicks; this is because we are automatically creating the GVRSDK (seems like a bug)
